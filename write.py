5b5ef2239e5171468dcc9091ff29c2a7c91eecde
Nicolas Bcg <bocage.nicolas11@gmail.com>
2023-05-16 12:35:41 +0000

    v1

--- write.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def main(filename,line):
-    fichier = open(filename, "a")
-    fichier.write(line)
-    fichier.close()
-
-if __name__ == "__main__":
-    main(sys.argv[1],sys.argv[2])
