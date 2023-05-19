fb35dd708b2b4f4004396410f1d37238e51cce49
Nicolas Bcg <bocage.nicolas11@gmail.com>
2023-05-19 07:50:04 +0000

    p

---
+++ createBackup.py
@@ -0,0 +1,14 @@
+import sys
+import os
+def main(model,nom,date,config):
+    path="backupsMicrotiks/"+model
+    if os.path.exists(path) == False:
+        os.mkdir(path)
+
+    filename=path+"/"+nom+date+".cfg"
+    fichier = open(filename, "w")
+    fichier.write(config)
+    fichier.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
